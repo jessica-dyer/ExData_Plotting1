286f731c135dc1865a82751f77d7b3bc583311e3
jessica-dyer <jessica.dyer@gmail.com>
2020-11-14 13:22:02 -0800

    Final graphs completed.

--- plot4.R
+++ plot4.R
@@ -2,14 +2,28 @@ source("data_loading.R")
 png("plot4.png", width = 480, height = 480)
 
 par(mfrow = c(2,2))
-par(mar = c(1,2,2,0))
-par(pty = "m")
-plot1 <- source("plot2.R")
+par(mar = c(2,0,2,0))
+par(pty = "s")
+plot1 <- plot(filtered_df$date_object, filtered_df$Global_active_power,  
+                       type = "l",
+                       ylab = "Global Active Power (kilowatts)",
+                       xlab = " ", 
+                       lty = 1, lwd = 1) 
 
 plot2 <- plot(filtered_df$date_object, filtered_df$Voltage, type = "l", 
               xlab = "", ylab = "Voltage",  lty = 1, lwd = 1, pch=16)
 
-plot3 <- source("plot3.R")
+plot3 <- plot(filtered_df$date_object, filtered_df$Sub_metering_1, type = "l",
+                 xlab = " ", ylab = "Energy sub metering",  lty = 1, lwd = 1)
+
+# Add a second & third line
+lines(filtered_df$date_object, filtered_df$Sub_metering_2, col = "red", type = "l")
+lines(filtered_df$date_object, filtered_df$Sub_metering_3, col = "blue", type = "l")
+
+
+# Add a legend to the plot and set legend lty
+legend("topright", legend = c("Sub Metering 1", "Sub Metering 2", "Sub Metering 3"),
+       col = c("black", "red", "blue"), lty = 1)
 
 plot4 <-  plot(filtered_df$date_object, filtered_df$Global_reactive_power, 
                xlab = "", ylab = "Global Reactive Power",  lty = 1, lwd = 1, type = "l")
